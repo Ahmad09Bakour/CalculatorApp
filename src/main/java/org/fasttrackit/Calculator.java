62323b7e938b1304f4543ec16c66955559066d88
Ahmad09Bakour <ahmad.hajbakour@gmail.com>
2018-12-09 23:36:32 +0200

    Establishing the first method

---
+++ src/main/java/org/fasttrackit/Calculator.java
@@ -0,0 +1,8 @@
+package org.fasttrackit;
+
+public class Calculator {
+    public double sumNumber (double x, double y) {
+        System.out.println("The result of adding the value x to y is: ");
+        return x + y;
+    }
+}
